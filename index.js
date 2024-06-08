18b9dff6ce3f116f4f290357b24fb4a3c7d35761
Sufyan Zaki <43870607+Sufyanzaki@users.noreply.github.com>
2024-06-08 16:17:04 +0500

    added CORS

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+const url='mongodb+srv://zainkkr5:w1nQquriQMzfyTHW@cluster0.470pnyp.mongodb.net/';
+import cors from 'cors'
 dotenv.config();
-const url='mongodb+srv://zainkkr5:w1nQquriQMzfyTHW@cluster0.470pnyp.mongodb.net/'
+
 mongoose
   //.connect(process.env.MONGO)
   .connect(url)
@@ -21,16 +23,15 @@ mongoose
   const __dirname = path.resolve();
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.use(cookieParser());
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
+  console.log('Server is running on port localhost:3000');
 });
 
-app.use('/', (req, res)=>res.send('Server is running on post 3000'));
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
